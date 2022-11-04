2bd7385ab0f4ccf396e13c6ace388536cb67bbdb
Daniel Martinez <daniel_ends@yahoo.com>
2022-11-04 12:12:11 -0600

    -removing old csvs and adding a singular one. Working csv data creation script

--- dataCreation/featuresCSV.py
+++ dataCreation/featuresCSV.py
@@ -9,11 +9,15 @@ listOfDir = os.listdir('../ArtSamples-150_files_each')
 for emotion in listOfDir:
     filesList = os.listdir('../ArtSamples-150_files_each/' + emotion)
     for image in filesList:
-        painting1 = Painting("currentImage", "C:\\Users\\Daniel Martinez\\PycharmProjects\\PaintingEmotion\\ExamplePaintings"
-                                             "\\test.jpg")
-        painting1.preprocessing()
-        painting1.calculateProperties()
-        with open('fixedLists\\' + emotion + '.csv', 'a') as fd:
-            writer = csv.writer(fd)
-            writer.writerow(painting1.getPropertiesList())
-
+        with open('csvTraining.csv', 'a') as fd:
+            if any(image in item for item in fd):
+                break
+            else:
+                writer = csv.writer(fd)
+                painting1 = Painting("currentImage", '../ArtSamples-150_files_each/' + emotion + '/' + image)
+                painting1.preprocessing()
+                painting1.calculateProperties()
+                data = painting1.getPropertiesList()
+                data.append(image)
+                data.append(emotion)
+                writer.writerow(data)
